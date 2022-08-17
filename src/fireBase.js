a6584fe8603447c3b4b2994a4b778f534a838f97
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2022-08-17 13:08:22 -0700

    connected Firebase and enabled google analytics.

---
+++ src/fireBase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBaiyYdNU1SGT4zoeYwK0e9ISB-Z3iqoaQ",
+    authDomain: "the-exchange-f6a6b.firebaseapp.com",
+    projectId: "the-exchange-f6a6b",
+    storageBucket: "the-exchange-f6a6b.appspot.com",
+    messagingSenderId: "947108411170",
+    appId: "1:947108411170:web:4e1529b26e3abcc834177b",
+    measurementId: "G-NLS334H3G2"
+  };
